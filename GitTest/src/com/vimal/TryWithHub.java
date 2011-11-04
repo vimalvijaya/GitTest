ef548fb064a448c7f200f1a4950aef006466a278
vimkumar <vimalvijayamani@gmail.com>
2011-11-04 16:10:20 -0700

    Second checking

--- GitTest/src/com/vimal/TryWithHub.java
+++ GitTest/src/com/vimal/TryWithHub.java
@@ -8,7 +8,7 @@ public class TryWithHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Going with Hub");
+		System.out.println("Going with Hub updating");
 
 	}
 
