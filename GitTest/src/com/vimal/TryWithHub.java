1a812cf7610c232263b08e8fc8bbd04e331b702b
vimkumar <vimalvijayamani@gmail.com>
2011-11-04 16:06:03 -0700

    Sample

---
+++ GitTest/src/com/vimal/TryWithHub.java
@@ -0,0 +1,15 @@
+package com.vimal;
+
+public class TryWithHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Going with Hub");
+
+	}
+
+}
